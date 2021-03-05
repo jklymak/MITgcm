7d1ea0b52fcd2b9ede620bbce763d26085a2d1bc
Jody Klymak <jklymak@gmail.com>
2021-03-05 13:10:01 -0800

    Add mixing

--- pkg/var_bot_drag/VAR_BOT_DRAG.h
+++ pkg/var_bot_drag/VAR_BOT_DRAG.h
@@ -16,25 +16,27 @@ C     2-dim. fields
      &  var_bot_dragu_linear, var_bot_dragv_linear,
      &  var_bot_dragu_quadratic, var_bot_dragv_quadratic,
      &  var_bot_stressu, var_bot_stressv,
-     &  var_bot_drag_fac
+     &  var_bot_worku, var_bot_workv,
+     &  var_bot_drag_fac, var_bot_Nsq
       _RL var_bot_dragu_linear(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL var_bot_dragv_linear(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL var_bot_dragu_quadratic(1-OLx:sNx+OLx,1-OLy:sNy+OLy,
      &                            nSx,nSy)
       _RL var_bot_dragv_quadratic(1-OLx:sNx+OLx,1-OLy:sNy+OLy,
      &                            nSx,nSy)
-      _RL var_bot_stressu(1-OLx:sNx+OLx,1-OLy:sNy+OLy,
-     &                            nSx,nSy)
-      _RL var_bot_stressv(1-OLx:sNx+OLx,1-OLy:sNy+OLy,
-     &                            nSx,nSy)
+      _RL var_bot_stressu(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL var_bot_stressv(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL var_bot_worku(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL var_bot_workv(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL var_bot_Nsq(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL var_bot_drag_fac(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
 
 C     Scalars:
       COMMON /VAR_BOT_DRAG_SCALARS/
      &  Var_Bot_Drag_LinNPow, Var_Bot_Drag_QuadNPow,
-     &  Var_Bot_Drag_NScale
+     &  Var_Bot_Drag_NScale, var_bot_drag_cfac
        _RL Var_Bot_Drag_LinNPow, Var_Bot_Drag_QuadNPow,
-     &     Var_Bot_Drag_NScale
+     &     Var_Bot_Drag_NScale, Var_Bot_Drag_cfac
 
 #endif /* ALLOW_VAR_BOT_DRAG */
 
