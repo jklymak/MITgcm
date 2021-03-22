98ac8418b6db2b0f17619273400b60fd38071369
Jody Klymak <jklymak@gmail.com>
2021-03-22 11:48:34 -0700

    Almost Working

--- pkg/var_bot_drag/VAR_BOT_DRAG.h
+++ pkg/var_bot_drag/VAR_BOT_DRAG.h
@@ -5,11 +5,11 @@
       COMMON /VAR_BOT_DRAG_FILES/
      &       Var_Bot_Drag_uLinFile, Var_Bot_Drag_vLinFile,
      &       Var_Bot_Drag_uQuadFile, Var_Bot_Drag_vQuadFile,
-     &       Var_Bot_Drag_FacFile
+     &       Var_Bot_Drag_VertFile
       CHARACTER*(MAX_LEN_FNAM)
      &       Var_Bot_Drag_uLinFile, Var_Bot_Drag_vLinFile,
      &       Var_Bot_Drag_uQuadFile, Var_Bot_Drag_vQuadFile,
-     &       Var_Bot_Drag_FacFile
+     &       Var_Bot_Drag_VertFile
 
 C     2-dim. fields
       COMMON /VAR_BOT_DRAG_FIELDS/
@@ -17,7 +17,8 @@ C     2-dim. fields
      &  var_bot_dragu_quadratic, var_bot_dragv_quadratic,
      &  var_bot_stressu, var_bot_stressv,
      &  var_bot_worku, var_bot_workv,
-     &  var_bot_drag_fac, var_bot_Nsq
+     &  var_bot_vert_scale, var_bot_Nsq,
+     &  var_bot_uscale, var_bot_vscale
       _RL var_bot_dragu_linear(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL var_bot_dragv_linear(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL var_bot_dragu_quadratic(1-OLx:sNx+OLx,1-OLy:sNy+OLy,
@@ -28,8 +29,10 @@ C     2-dim. fields
       _RL var_bot_stressv(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL var_bot_worku(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL var_bot_workv(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL var_bot_uscale(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      _RL var_bot_vscale(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
       _RL var_bot_nsq(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL var_bot_drag_fac(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL var_bot_vert_scale(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
 
 C     Scalars:
       COMMON /VAR_BOT_DRAG_SCALARS/
